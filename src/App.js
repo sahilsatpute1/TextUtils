550a3f5c2317783581e8c38d203f5408ac4b247c
sahilsatpute1 <64419151+sahilsatpute1@users.noreply.github.com>
2021-09-01 02:09:12 +0530

    Routing has been done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
-// import About from './Components/About';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm'
 import React, {useState} from 'react'; 
 import Alert from './Components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light')
@@ -36,12 +42,21 @@ function App() {
   return (
    <>
     {/* <Navbar title="TextUtils" aboutText="About TextUtils"/>   */}
+    <Router>
     <Navbar title="TextUtils" mode={mode}   toggleMode={toggleMode}/>  
     <Alert alert= {alert}/>
     <div className="container my-3">
-    <TextForm showAlert={showAlert} heading="Enter your text to analyze below" mode={mode}/>
-    {/* <About/> */}
+    <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+          <TextForm showAlert={showAlert} heading="Enter your text to analyze below" mode={mode}/>
+          </Route>
+        </Switch>
+    
     </div>
+    </Router>
    </>
   );
 }
