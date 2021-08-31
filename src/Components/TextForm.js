7bf253f3b7cf33c5d709a965076198dd4813c2ab
sahilsatpute1 <64419151+sahilsatpute1@users.noreply.github.com>
2021-09-01 02:36:18 +0530

    Routers has been added

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
@@ -65,71 +64,3 @@ export default function TextForm(props) {
     </>
     )
 }
-=======
-import React, {useState} from 'react'
-
-export default function TextForm(props) {
-    const handleUpClick = () => {
-        let newText = text.toUpperCase();
-        setText(newText);
-        props.showAlert("Converted to uppercase", "success");
-    }
-
-    const handleLoClick = () => {
-        let newText = text.toLowerCase();
-        setText(newText);
-        props.showAlert("Converted to lowercase", "success");
-    }
-
-    const handleClearClick = () => {
-        let newText = '';
-        setText(newText);
-        props.showAlert("Text has been cleared", "success");
-    }
-
-    const handleCopy = () => {
-        let text = document.getElementById('myBox');
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        props.showAlert("Text has been copied to clipboard", "success");
-    }
-
-    const handleOnChange = (event) => {
-           setText(event.target.value)
-    }
-
-    const handleExtraSpaces = () => {
-        let newText = text.split(/[ ]+/);
-        setText(newText.join(' '));
-        props.showAlert("Extra spaces has been removed", "success");
-    }
-
-
-    const [text, setText] = useState('');
-    // setText("New text") //correct way to change state
-
-
-    return (
-        <>
-    <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}}  id="myBox" rows="8"></textarea>
-        </div>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
-        <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy text</button>
-        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-    </div>
-    <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
-      <h2>Your text summary</h2>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
-      <p>{0.008 * text.split(" ").length} Minutes read</p>
-      <h2>Preview</h2>
-      <p>{text.length>0 ? text : "Enter something in the textbox above to preview it here "}</p>
-    </div>
-    </>
-    )
-}
->>>>>>> e821a3e (routing has been done for home and about)
