045182ec49634f7c8b516696abd189ab0d846f40
sahilsatpute1 <64419151+sahilsatpute1@users.noreply.github.com>
2021-08-31 19:52:29 +0530

    Changes for alert mode, favicon of the website has been changed

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm'
 import React, {useState} from 'react'; 
+import Alert from './Components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
 
   const toggleMode = () => {
     if(mode==='dark') {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success")
     }
     else {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success")
     }
   }
 
@@ -23,8 +37,9 @@ function App() {
    <>
     {/* <Navbar title="TextUtils" aboutText="About TextUtils"/>   */}
     <Navbar title="TextUtils" mode={mode}   toggleMode={toggleMode}/>  
+    <Alert alert= {alert}/>
     <div className="container my-3">
-    <TextForm heading="Enter your text to analyze below" mode={mode}/>
+    <TextForm showAlert={showAlert} heading="Enter your text to analyze below" mode={mode}/>
     {/* <About/> */}
     </div>
    </>
